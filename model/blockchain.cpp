5a13ccbf43b85eb2016b7db18494a7a30d425767
Jan Kotras <kotras@izon.cz>
2020-02-26 15:03:22 +0100

    init

---
+++ model/blockchain.cpp
@@ -0,0 +1,5 @@
+//
+// Created by honza on 25.2.20.
+//
+
+#include "blockchain.h"
---
+++ model/node.h
@@ -0,0 +1,15 @@
+//
+// Created by honza on 25.2.20.
+//
+
+#ifndef PROJECT_NODE_H
+#define PROJECT_NODE_H
+
+namespace ns3 {
+
+    class Node {
+
+    };
+}
+
+#endif //PROJECT_NODE_H
